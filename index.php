68bbf4b24e68084993e38727bb346b9b51eaa537
Christopher Kennedy <kennedysgarage@gmail.com>
2011-09-09 14:28:56 -0400

    added viewports for tablet

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 	<title>Wine Spectator Apps</title>
 	<meta name="description" content="">
 	<meta name="author" content="">
+	<meta name='viewport' content='width=device-width; initial-scale=1.0; maximum-scale=1.0;'>
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<link rel="shortcut icon" href="favicon.ico">
 	<link rel="apple-touch-icon" href="apple-touch-icon.png">
 	<link rel="stylesheet" href="css/style.css">
