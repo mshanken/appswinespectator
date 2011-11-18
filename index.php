4eedfbb3a25e8de18d1e85ad60c729693187078f
Christopher Kennedy <kennedysgarage@gmail.com>
2011-11-18 16:50:33 -0500

    remove scrips

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
 	</div><!--/container-->
 
 
-	<script src="js/script.js"></script>
+	<!--<script src="js/script.js"></script>-->
 	<!--[if lt IE 7 ]>
 	<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
 	<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
