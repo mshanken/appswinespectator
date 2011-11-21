a78a96b727a64f64fe13e4135971fd53ab24a8e2
Christopher Kennedy <kennedysgarage@gmail.com>
2011-11-21 10:11:08 -0500

    updated for responce

--- web/js/script.js
+++ web/js/script.js
@@ -1,64 +1,49 @@
-// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log=function(){log.history=log.history||[];log.history.push(arguments);if(this.console){console.log(Array.prototype.slice.call(arguments))}};
-(function(b){function c(){}for(var d="assert,clear,count,debug,dir,dirxml,error,exception,firebug,group,groupCollapsed,groupEnd,info,log,memoryProfile,memoryProfileEnd,profile,profileEnd,table,time,timeEnd,timeStamp,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
-{console.log();return window.console;}catch(err){return window.console={};}})());
-
 // Prefix for amazon
 yepnope.addPrefix('amazon', function(resourceObj) {
 	resourceObj.url = 'https://s3.amazonaws.com/toolkit.mshanken.com/' + resourceObj.url;
 	return resourceObj;
 });
-
-// Jquery
-yepnope([{
-	load: '//ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.js',
+var _gaq = [['_setAccount','UA-23484466-5'],['_trackPageview']];
+yepnope([
+{
+	load: '//ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js', // Jquery
 	complete: function () {
 		if (!window.jQuery) {
 			yepnope('amazon!toolkit/js/jquery.min.js');
 		}
 	}
-}, {
-	complete: function () {		
-		console.info('Loaded: jQuery');
+},
+{
+	test: Modernizr.mq('only all'), // Testing if browser supports mq
+	nope: {
+		'respondJS' : 'amazon!js/respond.min.js'
+	},
+	callback: {
+		'respondJS' : function(url, result, key){
+			console.info('Loaded: respondJS');
+		}
 	}
-}]);
-
-// Fancybox
-yepnope({
-	test: document.getElementById('fb'),
+},
+{
+	test: document.getElementById('fb'), // Fancybox
 	yep: {
 		'MousewheelJS': 'amazon!plugins/fancybox/jquery.mousewheel-3.0.4.pack.js',
 		'FancyboxJS': 'amazon!plugins/fancybox/jquery.fancybox-1.3.4.pack.js',
 		'FancyboxCSS': 'amazon!plugins/fancybox/jquery.fancybox-1.3.4.css'
 	},
 	callback: {
-		'MousewheelJS' : function(url, result, key){
-			console.info('Loaded: MousewheelJS');
-		},
 		'FancyboxJS' : function(url, result, key){
-			console.info('Loaded: FancyboxJS');
 			$("a#fb").fancybox();
-		},
-		'FancyboxCSS' : function(url, result, key){
-			console.info('Loaded: FancyboxCSS');
 		}
 	}
-});
-
-// Tipsy
-yepnope({
-	test: document.getElementsByClassName('tip').length,
-	yep: {
-		'TipsyJS': 'amazon!plugins/tooltip/tooltip.js',
-		'TipsyCSS': 'amazon!plugins/tooltip/tooltip.css'
-	},
-	callback: {
-		'TipsyJS' : function(url, result, key){
-			console.info('Loaded: TipsyJS');
-			$('.tip').tipsy({fade: true});
-		},
-		'TipsyCSS' : function(url, result, key){
-			console.info('Loaded: TipsyCSS');
-		}
+},
+{
+	load : 'ielt7!https://ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js',
+	callback : function () {
+		window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})});
 	}
-});
+},
+{
+	load : 'http://www.google-analytics.com/ga.js'
+}
+]);
