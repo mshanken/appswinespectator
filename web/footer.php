c056023f70268942f2fecc050c8c7b0a451284cd
Christopher Kennedy <kennedysgarage@gmail.com>
2012-06-12 15:05:01 -0400

    adding new pages to convert over to the new system

---
+++ web/footer.php
@@ -0,0 +1,17 @@
+	<footer>
+		<p>&copy; 2011 <a href="http://www.winespectator.com">Wine Spectator</a>. All Rights Reserved.</p>
+	</footer>
+	
+</div><!--/container-->
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.7.0.min.js">\x3C/script>')</script>
+<script defer src="https://s3.amazonaws.com/toolkit.mshanken.com/plugins/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+<script defer src="https://s3.amazonaws.com/toolkit.mshanken.com/plugins/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+<script defer src="js/script.js"></script>
+<!--[if lt IE 7 ]>
+<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+<![endif]-->
+</body>
+</html>
