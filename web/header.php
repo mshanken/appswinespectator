a7da45dd8ae1af55acd2431e6a9264f2dcec1073
Christopher Kennedy <kennedysgarage@gmail.com>
2012-07-03 15:47:40 -0400

    adding some php

--- web/header.php
+++ web/header.php
@@ -6,7 +6,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Wine Spectator Apps</title>
+	<title><?php echo $title ?> Wine Spectator Apps</title>
 	<meta name="description" content="">
 	<meta name="author" content="M. Shanken Communications, Inc.">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
