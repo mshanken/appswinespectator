bb31dcd135fe7241556603697b144b3352d6e594
Christopher Kennedy <kennedysgarage@gmail.com>
2012-07-11 09:50:13 -0400

    converting over to the new desing

---
+++ web/wineratingsplus.php
@@ -0,0 +1,36 @@
+<?php  $title = 'WineRatings+'; include 'header.php'; ?>
+
+	<div id="intro">
+		<div class="container">
+			<div class="g4">
+				    <img src="http://placehold.it/460x882"/>
+			</div><!--/.g4-->
+			<div class="g8">
+				<h2><img src="/img/app-icon.png"/> Vintage <span>Chart</span> +</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat </p>
+				<a href=""><img src="/img/app-store.png" id="app-store"/></a>			
+			</div><!--/.g8-->
+		</div><!--/.container-->
+	</div><!--/#intro-->
+	
+	<div class="container">
+	
+		<div id="feats" class="clearfix">
+			<div class="g4">
+				<img src="http://placehold.it/460x307"/>
+				<h3>Title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam nibh. Nunc varius facilisis eros. Sed erat. In in velit quis arcu ornare laoreet. Curabitur adipiscing luctus massa</p>
+			</div>
+			<div class="g4">
+				<img src="http://placehold.it/460x307"/>
+				<h3>Title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam nibh. Nunc varius facilisis eros. Sed erat. In in velit quis arcu ornare laoreet. Curabitur adipiscing luctus massa</p>
+			</div>
+			<div class="g4">
+				<img src="http://placehold.it/460x307"/>
+				<h3>Title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam nibh. Nunc varius facilisis eros. Sed erat. In in velit quis arcu ornare laoreet. Curabitur adipiscing luctus massa</p>
+			</div>
+		</div><!--/feats-->
+
+<?php include("footer.php"); ?>
