014c15dab1473f1900111fb37bf9891fc99d2265
Christoher Kennedy <kennedysgarage@gmail.com>
2012-08-30 09:58:41 -0400

    fixing the infieldlabel

--- web/js/script.js
+++ web/js/script.js
@@ -1,11 +1,11 @@
 //@codekit-prepend "mousewheel-3.0.6.pack.js";
 //@codekit-prepend "fancybox.pack.js";
-//@codekit-prepend "jquery.infieldlabel.min.js";
+//@codekit-prepend "infieldlabel.js";
 
 $(document).ready(function() {
 	// FancyBox
 	$(".lightbox").fancybox();
-	
+
 	// inFieldLabels
 	$("label").inFieldLabels();
 });
