86f0e51b1d148a1bc9de2d8ec2b363ed65496e63
Christoher Kennedy <kennedysgarage@gmail.com>
2012-08-31 13:02:17 -0400

    fixing the after effect for submit

--- web/js/script.js
+++ web/js/script.js
@@ -8,4 +8,12 @@ $(document).ready(function() {
 
 	// inFieldLabels
 	$("label").inFieldLabels();
+
+    // Newsletter
+    $("#signup input[type='submit']").click(function(event) {
+        event.preventDefault();
+        var this = $("#signup input[type='submit']");
+        $("#signup ul").parent().hide();
+        $("#submit").after('<p>Thanks for signing up. Make sure to follow on twitter at <a href="https://twitter.com/winespectator">@WineSpectator</a>.</p>');
+    });
 });
