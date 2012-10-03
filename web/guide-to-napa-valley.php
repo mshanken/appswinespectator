1359cfd33164e0c735a096f4467b7256576629d2
Christoher Kennedy <kennedysgarage@gmail.com>
2012-10-03 13:46:28 -0400

    cleaning up the files

--- web/guide-to-napa-valley.php
+++ web/guide-to-napa-valley.php
@@ -17,17 +17,17 @@
 	<div class="container">
 		<div id="feats" class="clearfix">
 			<div class="g4">
-				<a class="lightbox" href="img/napavalley/map_b.png"><img src="/img/napavalley/map_s.png"/></a>
+				<a class="lightbox" href="img/napavalley/1b.png"><img src="/img/napavalley/1s.png"/></a>
 				<h3>PLAN YOUR TRIP</h3>
 				<p>Find the hotels, restaurants, wineries and attractions that suit your interests and budget. Wine Spectator&lsquo;s California editors reviewed every venue included to ensure that they deliver the quality our readers expect. The convenient lists for each category can be quickly filtered by location, price range or other criteria. It&lsquo;s easy to plan your perfect itinerary!</p>
 			</div>
 			<div class="g4">
-				<a class="lightbox" href="img/napavalley/content_b.png"><img src="/img/napavalley/content_s.png"/></a>
+				<a class="lightbox" href="img/napavalley/2b.png"><img src="/img/napavalley/2s.png"/></a>
 				<h3>A Napa Valley Primer</h3>
 				<p>Get to know Napa Valley and its star player, Cabernet Sauvignon, with in-depth articles on the region&lsquo;s history, geography, appellations and wines. Stunning photos give you a unique view of the places that produce your favorite wines. Pop-up definitions quickly explain complex wine terms, and top chefs provide perfect recipe pairings for Cabernet.</p>
 			</div>
 			<div class="g4">
-				<a class="lightbox" href="img/napavalley/filter_b.png"><img src="/img/napavalley/filter_s.png"/></a>
+				<a class="lightbox" href="img/napavalley/3b.png"><img src="/img/napavalley/3s.png"/></a>
 				<h3>440+ Top-Scoring Wines</h3>
 				<p>You&lsquo;ll enjoy special access to Wine Spectator reviews spotlighting the most impressive wines and most important producers across Napa&lsquo;s appellations. Our editors selected 280 diverse Cabernets from recent vintages&mdash;plus Chardonnays, Merlots, Sauvignon Blancs and Zinfandels&mdash;that you can filter and sort by score, price and vintage.</p>
 			</div>
