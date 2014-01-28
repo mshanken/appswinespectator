89a349722bae9bfbe3145f8599afefe8ad0655e8
Steve Cannizzaro <cannizzaro75@gmail.com>
2014-01-28 14:25:12 -0500

    working on redoing apps homepage to reflect new wr plus design

--- web/index.php
+++ web/index.php
@@ -2,16 +2,20 @@
 
 	<div class="container">
 
-		<div class="g12">
-			<div id="home-wr">
-				<a href="/wineratingsplus" class="ss"><img src="/img/phone-wineratingsplus.png"/></a>
-				<div id="home-wr-copy">
-					<h2><a href="/wineratingsplus"><img src="/img/icon-wineratingsplus.png"/> Wine<span>Ratings</span>+</a></h2>
-					<p>Get convenient, expert advice when choosing wine for dinner, a party or another occasion. Wine Spectator's flagship Wine Ratings+ app provides quick access to all of our wine reviews&mdash;nearly 300,000. Designed for connoisseurs and casual drinkers, this comprehensive app for iPhone helps you make better choices, discover great values, learn about wine and stay updated on what&lsquo;s new.</p>
-					<a href="http://itunes.apple.com/us/app/vintagechart-by-wine-spectator/id381341648"><img src="/img/app-store.png" id="app-store"/></a>
-				</div><!--/home-wr-copy-->
+		
+			<div id="home-wr" class="clearfix">
+				<div class="g6">
+					<a href="/wineratingsplus" class="ss"><img src="/img/phone-wineratingsplus.png"/></a>
+				</div>
+				<div class="g6">
+					<div id="home-wr-copy">
+						<h2><a href="/wineratingsplus"><img src="/img/icon-wineratingsplus.png"/> Wine<span>Ratings</span>+</a></h2>
+						<p>Get convenient, expert advice when choosing wine for dinner, a party or another occasion. Wine Spectator's flagship Wine Ratings+ app provides quick access to all of our wine reviews&mdash;nearly 300,000. Designed for connoisseurs and casual drinkers, this comprehensive app for iPhone and iPad helps you make better choices, discover great values, learn about wine and stay updated on what&lsquo;s new.</p>
+						<a href="http://itunes.apple.com/us/app/vintagechart-by-wine-spectator/id381341648"><img src="/img/app-store.png" id="app-store"/></a>
+					</div><!--/home-wr-copy-->
+				</div>
 			</div><!--/home-wr-->
-		</div><!--/g12-->
+		
 
 		<div class="g12">
 			<hr/>
