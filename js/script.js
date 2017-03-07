9c77b399e670461d39697e34128d6c94489d74af
Edison Leon <eleon@mshanken.com>
2017-03-07 15:36:03 -0500

    merging updates from master

--- js/script.js
+++ js/script.js
@@ -7,22 +7,22 @@ $(document).ready(function() {
 	$(".lightbox").fancybox();
 
 	// inFieldLabels
-	$("label").inFieldLabels();
+	// $("label").inFieldLabels();
 
     // Newsletter
-    $("#signup input[type='submit']").click(function(event) {
-        event.preventDefault();
-        var this = $("#signup input[type='submit']");
-        $("#signup ul").parent().hide();
-        $("#submit").after('<p>Thanks for signing up. Make sure to follow on twitter at <a href="https://twitter.com/winespectator">@WineSpectator</a>.</p>');
-    });
+  $("#signup input[type='submit']").click(function(event) {
+      event.preventDefault();
+      var this = $("#signup input[type='submit']");
+      $("#signup ul").parent().hide();
+      $("#submit").after('<p>Thanks for signing up. Make sure to follow on twitter at <a href="https://twitter.com/winespectator">@WineSpectator</a>.</p>');
+  });
 
-    // resizing function - newWidth is percentage of content width as a decimal value
-    BCL.resizePlayer = function(newWidth) {
-      var $BCLcontainingBlock = $('#BCLcontainingBlock');
-      $BCLcontainingBlock.width($('#BCLbodyContent').width() * newWidth);
-      BCL.experienceModule.setSize($BCLcontainingBlock.width(),$BCLcontainingBlock.height());
-      BCL.currentPlayerWidth = newWidth;
-    }
+  // resizing function - newWidth is percentage of content width as a decimal value
+  BCL.resizePlayer = function(newWidth) {
+    var $BCLcontainingBlock = $('#BCLcontainingBlock');
+    $BCLcontainingBlock.width($('#BCLbodyContent').width() * newWidth);
+    BCL.experienceModule.setSize($BCLcontainingBlock.width(),$BCLcontainingBlock.height());
+    BCL.currentPlayerWidth = newWidth;
+  }
 
 });
